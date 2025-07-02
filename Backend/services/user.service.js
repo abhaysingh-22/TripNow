c12273d91611b19d985543dd03a4d9b00fe25cfa
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-03 02:41:15 +0530

    Implement user creation service with input validation

---
+++ Backend/services/user.service.js
@@ -0,0 +1,18 @@
+import User from "../models/user.model";
+
+export const createUser = async({
+    firstName, lastName, email, password
+}) => {
+    if(!firstName || !lastName || !email || !password) {
+        throw new Error("All fields are required");
+    }
+    const user = new User({
+        fullName: {
+            firstName,
+            lastName
+        },
+        email,
+        password
+    });
+    return await user.save();
+}
