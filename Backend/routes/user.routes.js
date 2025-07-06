aa0fb284b4c9fa3bbc18b313de8cdaa2eccb642d
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-06 15:28:52 +0530

    Refactor user routes to import both registerUser and loginUser from user.controller

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { body } from "express-validator"; // Importing express-validator for request validation it helps in validating the request body for user registration and login
-import registerUser from "../controllers/user.controller.js"; 
+import {registerUser, loginUser} from "../controllers/user.controller.js";
+// import loginUser from "../controllers/user.controller.js";
 
 const router = Router();
 
@@ -15,6 +16,17 @@ router.post(
       .withMessage("Password must be at least 6 characters long"),
   ],
   registerUser
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Valid email is required"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  loginUser
 );
 
 export default router;
