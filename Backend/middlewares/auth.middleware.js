afe76057369fd0ee3fb7fc9e42c0d64fb3ee9fbe
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-10 01:38:44 +0530

    Add captain authentication middleware to handle token verification and user retrieval

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import BlacklistToken from "../models/blacklistToken.model.js";
+import Captain from "../models/captain.model.js";
 
 const authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
@@ -31,4 +31,32 @@ const authUser = async (req, res, next) => {
   }
 };
 
-export default authUser;
+const authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Authentication token is required" });
+  }
+
+  const isBlacklisted = await BlacklistToken.findOne({ token: token });
+
+  if (isBlacklisted) {
+    return res
+      .status(401)
+      .json({ error: "Token is blacklisted, please log in again" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const captain = await Captain.findById(decoded._id);
+    if (!captain) {
+      return res.status(404).json({ error: "Captain not found" });
+    }
+    req.captain = captain;
+
+    return next();
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export { authUser, authCaptain };
