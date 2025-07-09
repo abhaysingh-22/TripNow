cad4b490c71308e22e00a738be0fafa13a983ba7
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-10 01:38:54 +0530

    Refactor captain routes to consolidate imports and improve structure

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { registerCaptain } from "../controllers/captain.controller.js";
-// import authCaptain from "../middlewares/auth.middleware.js";
+import {
+  registerCaptain,
+  loginCaptain,
+  getCaptainProfile,
+  logoutCaptain,
+} from "../controllers/captain.controller.js";
 const router = Router();
 import { body } from "express-validator";
+import { authCaptain } from "../middlewares/auth.middleware.js";
 
 router.post(
   "/register",
@@ -19,9 +24,26 @@ router.post(
     body("vehicle.capacity")
       .isNumeric()
       .withMessage("Vehicle capacity must be a number"),
-    body("vehicle.typeofVehicle").notEmpty().withMessage("Vehicle type is required"),
+    body("vehicle.typeofVehicle")
+      .notEmpty()
+      .withMessage("Vehicle type is required"),
   ],
   registerCaptain
 );
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  loginCaptain
+);
+
+router.get("/profile", authCaptain, getCaptainProfile);
+
+router.get("/logout", authCaptain, logoutCaptain);
 
 export default router;
