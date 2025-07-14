328eeaac6f0d6914dfa014b55e1ceffa84f9f67a
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-15 01:41:44 +0530

    feat: Add UserContext for managing user state across the application

---
+++ Frontend/src/context/UserContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+const UserContextProvider = ({ children }) =>    {
+    const [user, setUser] = useState({
+        email: "",
+        fullName: {
+            firstName: "",
+            lastName: ""
+        }
+    });
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export { UserContext };
