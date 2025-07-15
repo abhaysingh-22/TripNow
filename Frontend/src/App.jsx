74e1e0feee7952beebaa509faecc3c4a11b67e8c
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-16 02:50:08 +0530

    feat: Wrap Home and UserLogout routes with UserProtectedWrapper for enhanced access control

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,19 +9,32 @@ import Captainlogin from "./pages/Captainlogin";
 import CaptainLogout from "./pages/CaptainLogout";
 import CaptainSignup from "./pages/CaptainSignup";
 import Start from "./pages/Start";
+import UserProtectedWrapper from "./pages/UserProtectedWrapper";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Start />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/logout" element={<UserLogout />} />
         <Route path="/captain-login" element={<Captainlogin />} />
         <Route path="/captain-logout" element={<CaptainLogout />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectedWrapper>
+              <Home />
+            </UserProtectedWrapper>
+          }
+        />
+        <Route path="/users/logout" element={
+          <UserProtectedWrapper>
+            <UserLogout />
+          </UserProtectedWrapper>
+        } />
       </Routes>
       {/* <div className="bg-gray-800 text-white p-4 rounded">Hello world!</div> */}
     </>
