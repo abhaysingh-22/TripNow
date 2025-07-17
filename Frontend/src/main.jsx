17e3731e762335968e4c1db33846e341ad4a6ea3
myfirstrepo <abhaysingh6575@gmail.com>
2025-07-17 19:11:40 +0530

    feat: Nest UserContextProvider within CaptainProvider for improved context management

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserContextProvider } from "./context/UserContext.jsx";
+import { CaptainProvider } from "./context/CaptainContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UserContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserContextProvider>
+    <CaptainProvider>
+      <UserContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserContextProvider>
+    </CaptainProvider>
   </StrictMode>
 );
+
+// StrictMode is a wrapper component that activates additional checks and warnings for its descendants only in development mode (not in production).
