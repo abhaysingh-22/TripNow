6624c0ab2707334f0974e80a2709261650f22718
myfirstrepo <abhaysingh6575@gmail.com>
2025-08-04 01:36:25 +0530

    fix: Clean up formatting and comments in maps routes for better readability

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -1,15 +1,23 @@
 import express from "express";
 import mapsController from "../controllers/maps.controller.js";
 const router = express.Router();
-import {authUser} from "../middlewares/auth.middleware.js";
+import { authUser } from "../middlewares/auth.middleware.js";
 import { query } from "express-validator";
 
 router.get(
   "/geocode",
-  authUser,     // here we are authenticating the user becuase if not then we have to pay to google for the api requests
+  authUser, // here we are authenticating the user becuase if not then we have to pay to google for the api requests
   // Validate the query parameter 'address'
   query("address").isString().notEmpty(),
   mapsController.geocode
+);
+
+router.get(
+  "/distance-time",
+  authUser, // here we are authenticating the user becuase if not then we have to pay to google for the api requests
+  query("origin").isString().notEmpty(),
+  query("destination").isString().notEmpty(),
+  mapsController.getDistanceTime
 );
 
 export default router;
