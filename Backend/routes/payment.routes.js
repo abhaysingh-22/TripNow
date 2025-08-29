0420b1ad0002ced9c75b306497ffff78f3224d06
abhaysingh-22 <abhaysingh6575@gmail.com>
2025-08-29 18:51:59 +0530

    bug fixes

--- Backend/routes/payment.routes.js
+++ Backend/routes/payment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import paymentController from "../controllers/payment.controller";
+import paymentController from "../controllers/payment.controller.js";
 import { authUser } from "../middlewares/auth.middleware.js";
 
 const router = Router();
